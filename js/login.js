b670e31f891704d0bd81847b1e3b8fe294218d42
mfveronezi <mfveronezi@gmail.com>
2022-05-17 14:15:31 -0300

    Feat.: Tratamento de erro adicionado

--- js/login.js
+++ js/login.js
@@ -15,6 +15,20 @@ const botaoEntrar = document.querySelector('#botao-entrar');
 let listaInput = document.querySelectorAll('input');
 let listaLabel = document.querySelectorAll('label');
 
+function informaErro(msg) {
+    msgErro.setAttribute('style', 'transition: all .3s ease-out; display: inline-block; color: #ff0000; background-color: #ffaaaa;');
+    msgErro.innerHTML = `<strong>${msg}</strong>`;
+
+    msgSucesso.setAttribute('style', 'display: none');
+    msgSucesso.innerHTML = '';
+
+    email.setAttribute('style', 'transition: all 0.4s ease-out; box-shadow: 1px 1px 1.5px 1px #cc0000');
+    labelEmail.setAttribute('style', 'transition: all 0.4s ease-out; color: #cc0000');
+
+    senha.setAttribute('style', 'transition: all 0.4s ease-out; box-shadow: 1px 1px 1.5px 1px #cc0000');
+    labelSenha.setAttribute('style', 'transition: all 0.4s ease-out; color: #cc0000');
+}
+
 for (let i = 0; i < listaInput.length; i++) {
     let itemInput = document.getElementById(listaInput[i].getAttribute('id'));
     let itemLabel = document.getElementById(listaLabel[i].getAttribute('id'));
@@ -40,48 +54,42 @@ botaoEntrar.addEventListener('click', (event) => {
 
     listaUsuario = JSON.parse(localStorage.getItem('listaUsuario'));
 
-    listaUsuario.forEach((item) => {
-        if (email.value == item.emailCadastrado
-            && senha.value == item.senhaCadastrada) {
-            usuarioValido = {
-                nome: item.nomeCadastrado,
-                sobrenome: item.sobrenomeCadastrado,
-                email: item.emailCadastrado,
-                senha: item.senhaCadastrada
-            };
-        }
-    });
-
-    if (email.value == usuarioValido.email
-        && senha.value == usuarioValido.senha) {
-        msgSucesso.setAttribute('style', 'display: inline-block');
-        msgSucesso.innerHTML = '<strong>Logando...</strong>';
-
-        msgErro.setAttribute('style', 'display: none');
-        msgErro.innerHTML = '';
-
-        const token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
-        localStorage.setItem('token', token);
-
-        localStorage.setItem('usuarioLogado', JSON.stringify(usuarioValido));
-
+    if (listaUsuario == null) {
+        informaErro('E-mail ou Senha Incorreto');
         event.preventDefault();
-
-        setTimeout(() => { window.location.href = '../html/principal.html'; }, 3000);
     } else {
-        msgErro.setAttribute('style', 'transition: all .3s ease-out; display: inline-block; color: #ff0000; background-color: #ffaaaa;');
-        msgErro.innerHTML = '<strong>E-mail ou Senha incorreto</strong>';
+        listaUsuario.forEach((item) => {
+            if (email.value == item.emailCadastrado
+                && senha.value == item.senhaCadastrada) {
+                usuarioValido = {
+                    nome: item.nomeCadastrado,
+                    sobrenome: item.sobrenomeCadastrado,
+                    email: item.emailCadastrado,
+                    senha: item.senhaCadastrada
+                };
+            }
+        });
 
-        msgSucesso.setAttribute('style', 'display: none');
-        msgSucesso.innerHTML = '';
+        if (email.value == usuarioValido.email
+            && senha.value == usuarioValido.senha) {
+            msgSucesso.setAttribute('style', 'display: inline-block');
+            msgSucesso.innerHTML = '<strong>Logando...</strong>';
 
-        event.preventDefault();
+            msgErro.setAttribute('style', 'display: none');
+            msgErro.innerHTML = '';
 
-        email.setAttribute('style', 'transition: all 0.4s ease-out; box-shadow: 1px 1px 1.5px 1px #cc0000');
-        labelEmail.setAttribute('style', 'transition: all 0.4s ease-out; color: #cc0000');
+            const token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
+            localStorage.setItem('token', token);
 
-        senha.setAttribute('style', 'transition: all 0.4s ease-out; box-shadow: 1px 1px 1.5px 1px #cc0000');
-        labelSenha.setAttribute('style', 'transition: all 0.4s ease-out; color: #cc0000');
+            localStorage.setItem('usuarioLogado', JSON.stringify(usuarioValido));
+
+            event.preventDefault();
+
+            setTimeout(() => { window.location.href = '../html/principal.html'; }, 2000);
+        } else {
+            informaErro('E-mail ou Senha Incorreto');
+            event.preventDefault();
+        }
     }
 });
 
