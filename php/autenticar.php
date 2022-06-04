aff12b3886dc52e2588d72677fc7c38151a899fe
Reginaldo Donizete Felix <reginaldo.felix@b2wdigital.com>
2022-06-03 21:37:58 -0300

    Retornando o código Http Status Code de retorno conforme regra de validação

--- php/autenticar.php
+++ php/autenticar.php
@@ -11,6 +11,14 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
 
     // Buscar na tabela usuario o usuário que corresponde com os dados digitado no formulário
     $resultadoUsuario = mysqli_query($mysql, "SELECT TRUE logado, id, nome FROM usuario WHERE email = '$usuario' && senha = '$senha' LIMIT 1");
+
+    if($resultadoUsuario === FALSE) { 
+      http_response_code(500);
+      $retorno = array('logado' => 0, 'mensagem' => 'Falha interna, contacte o administrador. Erro: '.mysqli_error($mysql));
+      echo json_encode($retorno);
+      exit();
+    }
+
     $resultado = mysqli_fetch_assoc($resultadoUsuario);
 
     if (isset($resultado)) {
@@ -19,12 +27,14 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
         //Não foi encontrado um usuario na tabela usuário com os mesmos dados digitado no formulário
         //redireciona o usuario para a página de login
     } else {
+        http_response_code(404);
         $retorno = array('logado' => 0, 'mensagem' => 'Usuário não encontrado');
         echo json_encode($retorno);
         // echo 'Email: ' . $usuario . '  ';
         // echo 'Senha: ' . $senha;
     }
 } else {
+    http_response_code(400);
     $retorno = array('logado' => 0, 'mensagem' => 'E-mail ou senha incorreto');
     echo json_encode($retorno);
     if (!isset($_POST['email'])) {
@@ -38,3 +48,4 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
         echo 'Senha: ' . $_POST['senha'];
     }
 }
+
