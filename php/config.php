a506a2dcb1803fe8b3fb8fe631f502d720bb3c05
mfveronezi <mfveronezi@gmail.com>
2022-06-06 23:32:32 -0300

    Adicionada uma verificação à conexão com o banco de dados para que não seja necessário iniciar uma nova conexão, se houver mais de uma chamada

--- php/config.php
+++ php/config.php
@@ -1,20 +1,22 @@
 <?php
-$hostName = 'localhost';
-$userName = 'app_pacatos';
-$userPassword = 'p@th1506';
-$databaseName = 'pacatosteahouse';
-$mysql = new mysqli($hostName, $userName, $userPassword, $databaseName);
+if (!isset($mysql) || !$mysql->ping()) {
+    $hostName = 'localhost';
+    $userName = 'app_pacatos';
+    $userPassword = 'p@th1506';
+    $databaseName = 'pacatosteahouse';
+    $mysql = new mysqli($hostName, $userName, $userPassword, $databaseName);
 
-/* check connection */
-if ($mysql->connect_errno) {
-    //printf("Connect failed: %s\n", $mysql->connect_error);
-    header("Content-Type: application/json");
+    /* check connection */
+    if ($mysql->connect_errno) {
+        //printf("Connect failed: %s\n", $mysql->connect_error);
+        header("Content-Type: application/json");
 
-    http_response_code(500);
-    $retorno = array('logado' => 0, 'mensagem' => 'Falha interna, contacte o administrador. Erro: ' . $mysql->connect_error);
+        http_response_code(500);
+        $retorno = array('logado' => 0, 'mensagem' => 'Falha interna, contacte o administrador. Erro: ' . $mysql->connect_error);
 
-    echo json_encode($retorno);
-    exit();
+        echo json_encode($retorno);
+        exit();
+    }
+
+    $mysql->set_charset('utf8');
 }
-
-$mysql->set_charset('utf8');
