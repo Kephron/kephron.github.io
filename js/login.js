216ac86a7b7e9650f0a78277d9fe75725f9130c4
mfveronezi <mfveronezi@gmail.com>
2022-06-27 11:50:45 -0300

    Adicionado uma função para informar erro, alterado a forma como é enviado o formulário já preenchido, feito o tratamento dos códigos HTTP recebidos

--- js/login.js
+++ js/login.js
@@ -4,13 +4,13 @@ const labelEmail = document.querySelector('#label-email');
 const senha = document.querySelector('#senha');
 const labelSenha = document.querySelector('#label-senha');
 
+const formularioLogin = document.querySelector('.formulario-login');
+
 const mostraSenha = document.querySelector('.mostra-senha');
 const escondeSenha = document.querySelector('.esconde-senha');
 
 const msgErro = document.querySelector('#msg-erro');
 const msgSucesso = document.querySelector('#msg-sucesso');
-
-const botaoEntrar = document.querySelector('#botao-entrar');
 
 let listaInput = document.querySelectorAll('input');
 let listaLabel = document.querySelectorAll('label');
@@ -21,6 +21,23 @@ function pintaInput(input, color) {
 
 function pintaLabel(label, color) {
     label.setAttribute('style', `transition: all 0.3s ease-out; color: ${color}`);
+}
+
+function informaErro(msg) {
+    msgErro.setAttribute('style', 'transition: all .3s ease-out; display: inline-block; color: #ff0000; background-color: #ffaaaa;');
+    msgErro.innerHTML = `<strong>${msg}</strong>`;
+
+    msgSucesso.setAttribute('style', 'display: none');
+    msgSucesso.innerHTML = '';
+
+    email.blur();
+    senha.blur();
+
+    pintaLabel(labelEmail, '#cc0000');
+    pintaInput(email, '#cc0000');
+
+    pintaLabel(labelSenha, '#cc0000');
+    pintaInput(senha, '#cc0000');
 }
 
 for (let i = 0; i < listaInput.length; i++) {
@@ -37,17 +54,23 @@ for (let i = 0; i < listaInput.length; i++) {
     });
 }
 
-botaoEntrar.addEventListener('click', (event) => {
+formularioLogin.addEventListener('submit', (event) => {
     let verificaLogado = false;
 
-    let formData = new FormData();
-    formData.append('email', email.value);
-    formData.append('senha', senha.value);
+    let formData = new FormData(formularioLogin);
+    let objetoFormData = {};
+    formData.forEach((chave, valor) => {
+        objetoFormData[chave] = valor;
+    });
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "../php/autentica.php", true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
+            let regexStatusCodeFamilia400 = /^4[0-9]{2}/;
+            let regexStatusCodeFamilia500 = /^5[0-9]{2}/;
+
             if (this.status == 200) {
                 let resultado = JSON.parse(this.response);
 
@@ -64,41 +87,26 @@ botaoEntrar.addEventListener('click', (event) => {
                     msgErro.setAttribute('style', 'display: none');
                     msgErro.innerHTML = '';
 
-                    pintaLabel(labelEmail, '#006600');
-                    pintaInput(email, '#006600');
-
-                    pintaLabel(labelSenha, '#006600');
-                    pintaInput(senha, '#006600');
-
                     event.preventDefault();
 
                     setTimeout(() => { window.location.href = './principal.html'; }, 1000);
                 }
-            }
+            } else if (regexStatusCodeFamilia400.test(this.status)) {
+                informaErro("E-mail ou Senha Incorreto");
 
-            if (verificaLogado == false) {
-                msgErro.setAttribute('style', 'transition: all .3s ease-out; display: inline-block; color: #ff0000; background-color: #ffaaaa;');
-                msgErro.innerHTML = `<strong>E-mail ou Senha Incorreto</strong>`;
-
-                msgSucesso.setAttribute('style', 'display: none');
-                msgSucesso.innerHTML = '';
-
-                email.blur();
-                senha.blur();
-
-                pintaLabel(labelEmail, '#cc0000');
-                pintaInput(email, '#cc0000');
-
-                pintaLabel(labelSenha, '#cc0000');
-                pintaInput(senha, '#cc0000');
+                event.preventDefault();
+            } else if (regexStatusCodeFamilia500.test(this.status)) {
+                informaErro("Erro Interno");
 
                 event.preventDefault();
             }
         }
     }
-    xhr.send(formData);
+    // let encrypted = CryptoJS.AES.encrypt(JSON.stringify(formData), "EncryptionKey");
+    xhr.send(JSON.stringify(objetoFormData));
     event.preventDefault();
 });
+
 
 mostraSenha.addEventListener('click', () => {
     senha.setAttribute('type', 'text');
