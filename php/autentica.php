ddaad557228d13593ce96e5cb10696371d201a13
mfveronezi <mfveronezi@gmail.com>
2022-06-27 11:41:44 -0300

    Atualizado diversos nomes de variáveis para ter mais concordância e inserido validações quanto ao estado do usuário, caso esteja ativo ou não

--- php/autentica.php
+++ php/autentica.php
@@ -6,25 +6,34 @@ require '../src/Login.php';
 header("Content-Type: application/json");
 
 if (isset($_POST['email']) && isset($_POST['senha'])) {
-    $usuario = mysqli_real_escape_string($mysql, $_POST['email']);
+    $email = mysqli_real_escape_string($mysql, $_POST['email']);
     $senha = mysqli_real_escape_string($mysql, $_POST['senha']);
-    password_hash($senha, PASSWORD_DEFAULT);
+    $senhaCodificada = password_hash($senha, PASSWORD_DEFAULT);
 
-    $resultadoUsuario = mysqli_query($mysql, "SELECT id, nome FROM usuario WHERE email = '$usuario' && senha = '$senha' LIMIT 1");
+    $usuario = mysqli_query($mysql, "SELECT id_usuario, ativo FROM usuario WHERE email = '$email' && senha = '$senhaCodificada' LIMIT 1");
+    $resultadoUsuario = mysqli_fetch_assoc($usuario);
 
-    $resultado = mysqli_fetch_assoc($resultadoUsuario);
+    if (isset($resultadoUsuario['id_usuario']) && $resultadoUsuario['ativo'] == 1) {
+        $idUsuario = $resultadoUsuario['id_usuario'];
 
-    if (isset($resultado)) {
-        $objToken = new Token($resultado["id"]);
+        $cliente = mysqli_query($mysql, "SELECT nome FROM cliente WHERE id_usuario = '$idUsuario' LIMIT 1");
+        $resultadoCliente = mysqli_fetch_assoc($cliente);
+        $nomeCliente = $resultadoCliente['nome'];
+
+        $objToken = new Token($idUsuario);
 
         $usuarioLogado = new Login();
         $usuarioLogado->logado = true;
-        $usuarioLogado->id = $resultado["id"];
-        $usuarioLogado->nome = $resultado["nome"];
+        $usuarioLogado->id_usuario = $idUsuario;
+        $usuarioLogado->nome = $nomeCliente;
         $usuarioLogado->token = $objToken->token;
         $usuarioLogado->dataExpiracao = $objToken->dataExpiracao;
 
         echo json_encode($usuarioLogado);
+    } else if ((isset($resultadoUsuario['id_usuario']) && $resultadoUsuario['ativo'] != 1)) {
+        http_response_code(403);
+        $retorno = array('logado' => 0, 'mensagem' => 'Usuário bloqueado');
+        echo json_encode($retorno);
     } else {
         http_response_code(401);
         $retorno = array('logado' => 0, 'mensagem' => 'E-mail ou senha incorreto');
